c3342760ffc3679e36b8f8ddbfa7a399c8941b40
DearTanker <1122669+DearTanker@users.noreply.github.com>
2022-06-09 02:45:48 +0800

    Create Kill PUBG.bat

---
+++ Windows-Kitchen/Game/PUBG/Kill PUBG.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo 开始清理 PUBG 相关所有进程：
+echo ------------------------------------------------
+
+taskkill /f /im BEService.exe
+taskkill /f /im GameOverlayUI.exe
+taskkill /f /im TslGame.exe
+taskkill /f /im TslGame_BE.exe
+taskkill /f /im TslGame_UC.exe
+taskkill /f /im TslGame_ZK.exe
+taskkill /f /im ExecPubg.exe
+taskkill /f /im zksvc.exe
+taskkill /f /im iigw_server.exe
+taskkill /f /im ucldr_battlegrounds_gl.exe
+
+echo ------------------------------------------------
+echo 清理完成，请等待此窗口自动关闭后再运行PUBG。
+
+timeout /nobreak /t 5
